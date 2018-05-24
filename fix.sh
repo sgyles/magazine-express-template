4d3021e5f548da2246f57db353665d6f41b24f81
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-05-24 12:44:00 +0000

    added image folders

--- fix.sh
+++ fix.sh
@@ -1,85 +1,103 @@
-# only run this if you need to update 
-# the article.html
-# files across all folders in articles folder
+# # only run this if you need to update 
+# # the article.html
+# # files across all folders in articles folder
 
-rm -rf articles/nutrition/article1.html
-rm -rf articles/nutrition/article2.html
-rm -rf articles/nutrition/article3.html
-rm -rf articles/nutrition/article4.html
-rm -rf articles/nutrition/article5.html
-rm -rf articles/workouts/article1.html
-rm -rf articles/workouts/article2.html
-rm -rf articles/workouts/article3.html
-rm -rf articles/workouts/article4.html
-rm -rf articles/workouts/article5.html
-rm -rf articles/sports/article1.html
-rm -rf articles/sports/article2.html
-rm -rf articles/sports/article3.html
-rm -rf articles/sports/article4.html
-rm -rf articles/sports/article5.html
-rm -rf articles/technology/article1.html
-rm -rf articles/technology/article2.html
-rm -rf articles/technology/article3.html
-rm -rf articles/technology/article4.html
-rm -rf articles/technology/article5.html
-rm -rf articles/fashion/article1.html
-rm -rf articles/fashion/article2.html
-rm -rf articles/fashion/article3.html
-rm -rf articles/fashion/article4.html
-rm -rf articles/fashion/article5.html
-rm -rf articles/travel/article1.html
-rm -rf articles/travel/article2.html
-rm -rf articles/travel/article3.html
-rm -rf articles/travel/article4.html
-rm -rf articles/travel/article5.html
-rm -rf articles/mental-health/article1.html
-rm -rf articles/mental-health/article2.html
-rm -rf articles/mental-health/article3.html
-rm -rf articles/mental-health/article4.html
-rm -rf articles/mental-health/article5.html
-rm -rf articles/lifestyle/article1.html
-rm -rf articles/lifestyle/article2.html
-rm -rf articles/lifestyle/article3.html
-rm -rf articles/lifestyle/article4.html
-rm -rf articles/lifestyle/article5.html
+# rm -rf articles/nutrition/article1.html
+# rm -rf articles/nutrition/article2.html
+# rm -rf articles/nutrition/article3.html
+# rm -rf articles/nutrition/article4.html
+# rm -rf articles/nutrition/article5.html
+# rm -rf articles/workouts/article1.html
+# rm -rf articles/workouts/article2.html
+# rm -rf articles/workouts/article3.html
+# rm -rf articles/workouts/article4.html
+# rm -rf articles/workouts/article5.html
+# rm -rf articles/sports/article1.html
+# rm -rf articles/sports/article2.html
+# rm -rf articles/sports/article3.html
+# rm -rf articles/sports/article4.html
+# rm -rf articles/sports/article5.html
+# rm -rf articles/technology/article1.html
+# rm -rf articles/technology/article2.html
+# rm -rf articles/technology/article3.html
+# rm -rf articles/technology/article4.html
+# rm -rf articles/technology/article5.html
+# rm -rf articles/fashion/article1.html
+# rm -rf articles/fashion/article2.html
+# rm -rf articles/fashion/article3.html
+# rm -rf articles/fashion/article4.html
+# rm -rf articles/fashion/article5.html
+# rm -rf articles/travel/article1.html
+# rm -rf articles/travel/article2.html
+# rm -rf articles/travel/article3.html
+# rm -rf articles/travel/article4.html
+# rm -rf articles/travel/article5.html
+# rm -rf articles/mental-health/article1.html
+# rm -rf articles/mental-health/article2.html
+# rm -rf articles/mental-health/article3.html
+# rm -rf articles/mental-health/article4.html
+# rm -rf articles/mental-health/article5.html
+# rm -rf articles/lifestyle/article1.html
+# rm -rf articles/lifestyle/article2.html
+# rm -rf articles/lifestyle/article3.html
+# rm -rf articles/lifestyle/article4.html
+# rm -rf articles/lifestyle/article5.html
 
-cp article.html articles/nutrition/article1.html
-cp article.html articles/nutrition/article2.html
-cp article.html articles/nutrition/article3.html
-cp article.html articles/nutrition/article4.html
-cp article.html articles/nutrition/article5.html
-cp article.html articles/workouts/article1.html
-cp article.html articles/workouts/article2.html
-cp article.html articles/workouts/article3.html
-cp article.html articles/workouts/article4.html
-cp article.html articles/workouts/article5.html
-cp article.html articles/sports/article1.html
-cp article.html articles/sports/article2.html
-cp article.html articles/sports/article3.html
-cp article.html articles/sports/article4.html
-cp article.html articles/sports/article5.html
-cp article.html articles/technology/article1.html
-cp article.html articles/technology/article2.html
-cp article.html articles/technology/article3.html
-cp article.html articles/technology/article4.html
-cp article.html articles/technology/article5.html
-cp article.html articles/fashion/article1.html
-cp article.html articles/fashion/article2.html
-cp article.html articles/fashion/article3.html
-cp article.html articles/fashion/article4.html
-cp article.html articles/fashion/article5.html
-cp article.html articles/travel/article1.html
-cp article.html articles/travel/article2.html
-cp article.html articles/travel/article3.html
-cp article.html articles/travel/article4.html
-cp article.html articles/travel/article5.html
-cp article.html articles/mental-health/article1.html
-cp article.html articles/mental-health/article2.html
-cp article.html articles/mental-health/article3.html
-cp article.html articles/mental-health/article4.html
-cp article.html articles/mental-health/article5.html
-cp article.html articles/lifestyle/article1.html
-cp article.html articles/lifestyle/article2.html
-cp article.html articles/lifestyle/article3.html
-cp article.html articles/lifestyle/article4.html
-cp article.html articles/lifestyle/article5.html
+# cp article.html articles/nutrition/article1.html
+# cp article.html articles/nutrition/article2.html
+# cp article.html articles/nutrition/article3.html
+# cp article.html articles/nutrition/article4.html
+# cp article.html articles/nutrition/article5.html
+# cp article.html articles/workouts/article1.html
+# cp article.html articles/workouts/article2.html
+# cp article.html articles/workouts/article3.html
+# cp article.html articles/workouts/article4.html
+# cp article.html articles/workouts/article5.html
+# cp article.html articles/sports/article1.html
+# cp article.html articles/sports/article2.html
+# cp article.html articles/sports/article3.html
+# cp article.html articles/sports/article4.html
+# cp article.html articles/sports/article5.html
+# cp article.html articles/technology/article1.html
+# cp article.html articles/technology/article2.html
+# cp article.html articles/technology/article3.html
+# cp article.html articles/technology/article4.html
+# cp article.html articles/technology/article5.html
+# cp article.html articles/fashion/article1.html
+# cp article.html articles/fashion/article2.html
+# cp article.html articles/fashion/article3.html
+# cp article.html articles/fashion/article4.html
+# cp article.html articles/fashion/article5.html
+# cp article.html articles/travel/article1.html
+# cp article.html articles/travel/article2.html
+# cp article.html articles/travel/article3.html
+# cp article.html articles/travel/article4.html
+# cp article.html articles/travel/article5.html
+# cp article.html articles/mental-health/article1.html
+# cp article.html articles/mental-health/article2.html
+# cp article.html articles/mental-health/article3.html
+# cp article.html articles/mental-health/article4.html
+# cp article.html articles/mental-health/article5.html
+# cp article.html articles/lifestyle/article1.html
+# cp article.html articles/lifestyle/article2.html
+# cp article.html articles/lifestyle/article3.html
+# cp article.html articles/lifestyle/article4.html
+# cp article.html articles/lifestyle/article5.html
+
+mkdir articles/nutrition/images
+mkdir articles/workouts/images
+mkdir articles/sports/images
+mkdir articles/technology/images
+mkdir articles/fashion/images
+mkdir articles/travel/images
+mkdir articles/mental-health/images
+mkdir articles/lifestyle/images
+
+touch articles/nutrition/images/README.txt
+touch articles/workouts/images/README.txt
+touch articles/sports/images/README.txt
+touch articles/technology/images/README.txt
+touch articles/fashion/images/README.txt
+touch articles/travel/images/README.txt
+touch articles/mental-health/images/README.txt
+touch articles/lifestyle/images/README.txt
